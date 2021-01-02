deee572ae963beeb792007e0e111c8b8a835a1c6
TatyanaPilke <kykla.mc2@gmail.com>
2021-01-02 21:35:47 +0200

    added section faq, footer

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,21 @@
 $(function () {
 
+  $.fn.accordion = function() {
+    const trigger = $(this).find('.accordion-trigger');
+    const collapse = $(this).find('.accordion-collapse');
+  
+    $(trigger).each(function(){
+      $(this).on('click', function(){
+        $(this).siblings('.accordion-collapse').slideToggle();
+        $(this).toggleClass('accordion-open');
+        $(this).toggleClass('active');
+        $(this).parent().siblings('.accordion-item').find('.accordion-trigger').removeClass('accordion-open');
+        $(this).parent().siblings('.accordion-item').find('.accordion-collapse').slideUp();
+      });
+    });
+  }
+  
+  $('.accordion').accordion();
 
   var swiper = new Swiper('.swiper-container', {
     hashNavigation: {
