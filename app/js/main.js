d0d0bc1b986132db6839e37f7375adb7421d2bfb
TatyanaPilke <kykla.mc2@gmail.com>
2021-01-17 19:07:47 +0200

    added datepicker, select2

--- app/js/main.js
+++ app/js/main.js
@@ -13,11 +13,12 @@ $(function () {
         $(this).parent().siblings('.accordion-item').find('.accordion-collapse').slideUp();
       });
     });
-  }
+  };
 
   $('.accordion').accordion();
 
-  var swiper = new Swiper('.swiper-container', {
+  
+  let swiper = new Swiper('.swiper-container', {
     hashNavigation: {
       watchState: true,
     },
@@ -42,4 +43,36 @@ $(function () {
     }
   }
 
+  $(document).ready(function(){
+    $( ".select2" ).select2 ({ 
+      placeholder: "Выберите направление",
+    });
+  });
+
+  $( function() {
+    $( "#datepicker" ).datepicker();   
+    
+    jQuery(function ($) {
+        $.datepicker.regional['ru'] = {
+            closeText: 'Закрыть',
+            prevText: '&#x3c;Пред',
+            nextText: 'След&#x3e;',
+            currentText: 'Сегодня',
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+            dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+            dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            weekHeader: 'Нед',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['ru']);
+    });
+  });
 });
