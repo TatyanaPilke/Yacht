94cc4c5cf6eb32d2a8939e5e638b773ec3297a7c
TatyanaPilke <kykla.mc2@gmail.com>
2021-04-24 20:50:21 +0300

    added burger, adaptive main page

--- app/js/main.js
+++ app/js/main.js
@@ -96,7 +96,10 @@
     });
   });
 
-  // start datepicker 
+  // end datepicker 
+
+
+// start scroll on click 
 
   $(document).ready(function () {
     $("#header-menu").on("click", "a", function (event) {
@@ -108,3 +111,41 @@
       }, 1500);
     });
   });
+  
+// end scroll on click 
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
